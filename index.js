bf5dba31ce68ebd16f20ed11db48dd46623e4488
AyuburRahman <aiburrahman17@gmail.com>
2022-12-12 00:19:34 +0600

    added crud

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const bodyParser= require('body-parser')
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = 5000
+
+
 app.use(cors());
+app.use(bodyParser.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6jqkzzd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6jqkzzd.mongodb.net/moontech?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 const run = async () => {
   try {
